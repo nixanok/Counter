e7e6090ebab970001708cb783e1d25b05a04a5c0
nixanok <mrfreemen555@gmail.com>
2024-04-20 15:09:59 +0300

    completed

--- Counter/HistoryEvent.swift
+++ Counter/HistoryEvent.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class HistoryEvent {
+    let date : Date
+    let type : EventType
+    
+    init(date: Date, type: EventType) {
+        self.date = date
+        self.type = type
+    }
+}
+
+enum EventType : String {
+    case increment = "значение изменено на +1"
+    case decrement = "значение изменено на -1"
+    case failedDecrement = "попытка уменьшить значение счётчика ниже 0"
+    case reset = "значение сброшено"
+}
--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var counterUILabel: UILabel!
+    
+    @IBOutlet weak var counterIncrementUIButton: UIButton!
+    
+    @IBOutlet weak var counterDecrementUIButton: UIButton!
+    
+    @IBOutlet weak var historyEventsUITextView: UITextView!
+    
+    @IBOutlet weak var resetUIButton: UIButton!
+    
+    private var events : [HistoryEvent] = []
+    
+    private var score : Int = 0
+    
+    var dateFormatter:  DateFormatter {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return dateFormatter
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterUILabel.text = "0"
+        counterIncrementUIButton.setTitle("+1", for: .normal)
+        counterDecrementUIButton.setTitle("-1", for: .normal)
+        
+        resetUIButton.setTitle("Обнулить", for: .normal)
+        
+        counterIncrementUIButton.backgroundColor = .red
+        counterDecrementUIButton.backgroundColor = .blue
+        
+        historyEventsUITextView.text = ""
+        historyEventsUITextView.isEditable = false
     }
-
+    
+    @IBAction func counterIncrementUIButtonDidTap(_ sender: UIButton) {
+        score += 1
+        let historyEvent = HistoryEvent(date: Date.now, type: .increment)
+        updateScore(with: historyEvent)
+    }
+    
+    @IBAction func counterDecrementUIButtonDidTap(_ sender: UIButton) {
+        let historyEvent : HistoryEvent
+        if score > 0 {
+            score -= 1
+            historyEvent = HistoryEvent(date: Date.now, type: .decrement)
+        } else {
+            historyEvent = HistoryEvent(date: Date.now, type: .failedDecrement)
+        }
+        updateScore(with: historyEvent)
+    }
+    
+    @IBAction func resetCounterUIButtonDidTap(_ sender: UIButton) {
+        score = 0
+        let historyEvent = HistoryEvent(date: Date.now, type: .reset)
+        updateScore(with: historyEvent)
+    }
+    
+    private func updateScore(with event : HistoryEvent) {
+        let textCounter = "Значение счетчика : "
+        counterUILabel.text = textCounter + String(score)
+        print("LOG UPDATE new score : \(score)")
+        events.append(event)
+        var text : String = ""
+        for event in events {
+            text += "[\(dateFormatter.string(from: event.date))]: \(event.type.rawValue)\n"
+        }
+        historyEventsUITextView.text = text
+        historyEventsUITextView.contentOffset = CGPoint(x: 0, y: historyEventsUITextView.contentSize.height - historyEventsUITextView.frame.size.height)
+    }
 
 }
 
